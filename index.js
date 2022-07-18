c6912abc738f0496c594a53b9f4cddc4255cc069
JubayerDev <ajashes858@gmail.com>
2022-07-18 11:43:25 +0600

    server connected to mongodb

--- index.js
+++ index.js
@@ -2,15 +2,42 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
+require('dotenv').config()
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
-app.use(cors())
+//! Warning! Do not use in production
+app.use(cors({
+    origin: '*'
+}))
 app.use(express.json())
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.d1snq.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect()
+        const serviceCollection = client.db('cleanCo').collection('service')
+
+        app.get('/service', async (req, res) => {
+            const query = {};
+            const service = await serviceCollection.find(query).toArray();
+            res.send(service)
+        })
+    }
+    finally {
+        // await client.close()
+    }
+    console.log('DB Connected');
+}
+run().catch(console.dir)
 
 
 app.get('/', (req, res) => {
     res.send('Server Runnning')
 })
 
-app.listen(port,() => {
+app.listen(port, () => {
     console.log('listenning to port: ', port);
 })
